d89e8f6addfcefc759bbefd64e2d166a8cd9e672
Muzafar Ahmed <ahmed.muzafar@gmail.com>
2020-01-14 21:45:36 +0100

    test environment setup, in RED

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 module.exports = Phrase;
 
+//Gives all strings a reverse capability
+String.prototype.reverse = function reverse() {
+  return Array.from(this).reverse().join("");
+};
+
 //Create a Phrase object
 function Phrase (content) {
   this.content = content;
@@ -9,8 +14,14 @@ function Phrase (content) {
     return this.content.toLowerCase();
   }
 
+  // Returns letters only from string.
+  this.letters = function letters() {
+    return this.content;  //stub return value
+  }
+
   // Returns true for palindrome, false otherwise
   this.palindrome = function palindrome() {
     return this.processedContent() === this.processedContent().reverse();
   }
+
 }
